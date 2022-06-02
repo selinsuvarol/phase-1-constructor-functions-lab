9f9a1e6fc56f2e2ac536b8e76e90777d93b5b082
Selin Su Varol <selinsuvarol@gmail.com>
2022-06-03 00:14:49 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+// a. Scooter with year, color, and model properties:
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+  
+  // b. Driver with name, age, and experience properties:
+  function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+  
+  // c. PickupLocation with address and city properties:
+  function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+  }
